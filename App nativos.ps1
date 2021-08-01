2f2f8bdf6e157ca9d3238f2741a2ab1b678a1ba3
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2021-08-01 10:42:31 -0300

    desativando apps nativos

---
+++ App nativos.ps1
@@ -0,0 +1,11 @@
+﻿<#mostrar app nativos instalados#>
+
+Get-AppxPackage | Select Name, PackageFullName
+
+<#para remover algum programa dessa lista, use o seguinte comando
+
+
+Get-AppxPackage "NomedoApp" | Remove-AppxPackage
+
+
+#>
