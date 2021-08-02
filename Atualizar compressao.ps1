fba217570a004006fc6475aafcf66819e474af46
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2021-08-02 09:41:29 -0300

    Add files via upload

---
+++ Atualizar compressao.ps1
@@ -0,0 +1,4 @@
+﻿Compress-Archive -Path C:\Reference -Update -DestinationPath C:\Archives\Draft.Zip
+
+<#Este exemplo atualiza um arquivo existente, Draft.Zip, no diretório C: \ Archives.
+Neste exemplo, o arquivo existente contém o diretório raiz e seus arquivos e subdiretórios.#>
