21a7c838147d40ac1efe91d7dba4aef3636efcc7
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2021-08-02 09:43:01 -0300

    Add files via upload

---
+++ backup log.ps1
@@ -0,0 +1,4 @@
+﻿$server = hostname
+Get-WinEvent -LogName system -ComputerName $server | where {($_.ID -eq 1074) -or ($_.ID -eq 6008) -or ($_.ID -eq 41 )} | select -First 3 | fl 
+
+<#Backup de log de reinicialização do servidor remoto#>
