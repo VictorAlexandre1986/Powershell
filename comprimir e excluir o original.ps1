1e4190c809282480a59f75fd45fe7bb7e4eee766
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2021-08-04 14:39:24 -0300

    Add files via upload

---
+++ comprimir e excluir o original.ps1
@@ -0,0 +1,8 @@
+﻿$compress = @{
+Path= "C:\Users\Victor\Downloads\*"
+CompressionLevel = "Fastest"
+DestinationPath = "C:\Users\Victor\Downloads\teste"
+}
+Compress-Archive @compress
+
+<#o uso do asteriscos compacta para um arquivo e deopis exlcui o original#>
