45aed41989901e897d296a6f4901fb688cf060e8
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2021-08-04 14:38:57 -0300

    Add files via upload

---
+++ Comprimir de outra maneira com recursao.ps1
@@ -0,0 +1,8 @@
+﻿Get-ChildItem -Path C:\Users\Victor\Downloads\Eletronica -Recurse |
+  Compress-Archive -DestinationPath C:\Users\Victor\Downloads\teste.zip
+
+  <#O diretório C: \ TestLog não contém nenhum arquivo. Ele contém um subdiretório chamado testsub que contém o arquivo testlog.txt.
+
+Get-ChildItem usa o parâmetro Path para especificar o diretório raiz, C: \ TestLog. O parâmetro Recurse processa os arquivos e diretórios. Um objeto DirectoryInfo é criado para o testingub e um objeto FileInfo testlog.txt.
+
+Cada objeto é enviado pelo pipeline para o Compress-Archive. O DestinationPath especifica o local para o arquivo morto. O parâmetro Path não está especificado porque os objetos de pipeline são recebidos na posição de parâmetro 0.#>
