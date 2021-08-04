30a01b08ba217446649e8f6e8595d53b561ff9f4
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2021-08-04 14:39:44 -0300

    Add files via upload

---
+++ Comprimir mais de um arquivo.ps1
@@ -0,0 +1,6 @@
+﻿$compress = @{
+Path= "C:\Users\Victor\Downloads\ebejr01.pdf","C:\Users\Victor\Downloads\ebejr02.pdf"
+CompressionLevel = "Fastest"
+DestinationPath = "C:\Users\Victor\Downloads\teste"
+}
+Compress-Archive @compress
