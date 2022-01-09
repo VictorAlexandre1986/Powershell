5bce1174663d2029d410bc3d09bcb6164811d69e
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2022-01-09 18:36:58 -0300

    Add files via upload

---
+++ Politica de execucao.ps1
@@ -0,0 +1,14 @@
+﻿#obtendo a politica de execução de scripts
+Get-ExecutionPolicy
+
+
+#alterando a politica de execução de scripts
+Set-ExecutionPolicy
+
+#Restricted: não permite que nenhum arquivo seja executado isso inclui arquivos de configuração
+#AllSigned: Permite a execução de arquivos, porém necessita que os arquivos sejam assinados por um publicador confiável.
+#Unrestricted: Scripts não assinados podem rodar.
+#Bypass: Nada é bloqueado e não emite warnings ou prompts
+
+
+
